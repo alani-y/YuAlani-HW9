c3309cf97614a0bd50691274a8f3e6d5b31dac70
Alani Yu <alani.yu79@gmail.com>
2025-04-15 17:41:38 -0500

    intial H29 commit

--- YuAlani-HW9/ViewController.swift
+++ YuAlani-HW9/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // the sizes of the cell
+    let cellXSize = 0
+    let cellYSize = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
